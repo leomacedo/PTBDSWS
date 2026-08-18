bfdbe47889c35428a6d2adf325df667460c54272
Leo-Macedo <leonardo.aurieni@etec.sp.gov.br>
2026-08-17 22:22:28 -0300

    aat avaliacao 3

--- hello.py
+++ hello.py
@@ -1,30 +1,25 @@
-from datetime import datetime
-from flask import Flask, render_template, request
-from flask_moment import Moment
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-moment = Moment(app)
+app.config['SECRET_KEY'] = 'chave-secreta-para-formularios'
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
-
-@app.route('/identificacao/<nome>/<prontuario>/<instituicao>')
-def identificacao(nome, prontuario, instituicao):
-    return render_template('identificacao.html', nome=nome, prontuario=prontuario, instituicao=instituicao)
-
-@app.route('/contextorequisicao')
-def contexto_requisicao():
-    nome = "Leonardo Macêdo Aurieni"
-    user_agent = request.user_agent.string
-    
-    # Pega o IP correto sem quebrar no PythonAnywhere
-    ip_remoto = request.headers.get('X-Forwarded-For', request.remote_addr)
-    if ip_remoto and ',' in ip_remoto:
-        ip_remoto = ip_remoto.split(',')[0]
-        
-    host = request.host
-    return render_template('contexto.html', nome=nome, user_agent=user_agent, ip_remoto=ip_remoto, host=host)
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
