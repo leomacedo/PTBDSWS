3ad1ffbbf5ffbcab41d6727eb2e0912962b6045a
Leo-Macedo <leonardo.aurieni@etec.sp.gov.br>
2026-09-07 17:05:33 -0300

    att av 6

--- hello.py
+++ hello.py
@@ -1,24 +1,27 @@
 import os
+
 from flask import Flask, render_template, session, redirect, url_for
 from flask_wtf import FlaskForm
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate  # 1. Importar a extensão
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = 'chave-secreta-para-formularios'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)  # 2. Inicializar o Migrate vinculando com app e db
+migrate = Migrate(app, db)
 
-# Modelos do Banco de Dados
+
 class Role(db.Model):
     __tablename__ = 'roles'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     users = db.relationship('User', backref='role', lazy='dynamic')
@@ -26,8 +29,10 @@ class Role(db.Model):
     def __repr__(self):
         return f'<Role {self.name}>'
 
+
 class User(db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
@@ -35,32 +40,73 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.username}>'
 
-# Formulário
+
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-# Contexto para o shell
+
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
 
+
+def obter_role(nome):
+    role = Role.query.filter_by(name=nome).first()
+
+    if role is None:
+        role = Role(name=nome)
+        db.session.add(role)
+        db.session.commit()
+
+    return role
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
+
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
+        nome = form.name.data.strip()
+
+        user = User.query.filter_by(username=nome).first()
+
         if user is None:
-            user = User(username=form.name.data)
+            total_usuarios = User.query.count()
+
+            if total_usuarios == 0:
+                role = obter_role('Administrator')
+            else:
+                role = obter_role('User')
+
+            user = User(
+                username=nome,
+                role=role
+            )
+
             db.session.add(user)
             db.session.commit()
+
             session['known'] = False
+
         else:
             session['known'] = True
-        session['name'] = form.name.data
+
+        session['name'] = nome
         form.name.data = ''
+
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
+
+    usuarios = User.query.order_by(User.id.asc()).all()
+
+    return render_template(
+        'index.html',
+        form=form,
+        name=session.get('name'),
+        known=session.get('known', False),
+        usuarios=usuarios
+    )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
